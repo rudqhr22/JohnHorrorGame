82b74727c5405ffa5dd081fe93c8a6f9cda61f62
rudqhr22 <rudqhr22@naver.com>
2022-10-27 09:43:32 +0900

    Waypoint & Navigation Add

--- Assets/Scripts/WaypointPatrol.cs
+++ Assets/Scripts/WaypointPatrol.cs
@@ -17,7 +17,7 @@ public class WaypointPatrol : MonoBehaviour
 
     void Update()
     {
-        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
         {
             m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
             navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
